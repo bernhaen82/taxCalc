69091bd4a5400c7215966e62ee5ae1f8a1958638
Ethan B <bernhaen@mail.uc.edu>
2023-09-17 13:20:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args){
+    int price= 100;
+    double salesTax= 0.05;
+    double priceTax= 0;
+
+    System.out.println (" Your price of your purchase is " + price);
+    priceTax= salesTax * price;
+    System.out.println("Your sales tax is " + priceTax );
+
+
+        System.out.println("!");
+    }
+}
